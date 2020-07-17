25892daf49239de3ecb03d419ac7df6b340eaa90
rriku <68044945+rriku@users.noreply.github.com>
2020-07-17 17:29:14 +0900

    Update test.js

--- creatives/test.js
+++ creatives/test.js
@@ -1,6 +1,31 @@
-class SimidTest extends BaseSimidCreative {
+class SimidController extends BaseSimidCreative {
+  // コンストラクタ
   constructor() {
+    // セッション開始
     super();
+  }
+
+  // アンケートスキップ
+  skip(){
+    console.log("skip");
     this.simidProtocol.sendMessage(CreativeMessage.REQUEST_SKIP);
   }
+
 }
+
+
+
+
+// メイン処理ここから
+const simidController = new SimidController();
+simidController.ready();
+
+
+// ボタン処理
+// スキップ
+$(function(){
+	$("#button_skip").click(function(){
+      simidController.skip();
+    	return false;
+    });
+});
