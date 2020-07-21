cd95564b5c1d0812a6d04ab2b1bd2900ee5e6f5c
rriku <68044945+rriku@users.noreply.github.com>
2020-07-21 16:52:07 +0900

    Add files via upload

--- creatives/test.js
+++ creatives/test.js
@@ -13,12 +13,46 @@ class SimidController extends BaseSimidCreative {
 
 }
 
-
+var timestamp = new Date().getTime();
 
 
 // メイン処理ここから
 const simidController = new SimidController();
 simidController.ready();
+main();
+
+
+
+function main(){
+  new Vue({
+    el: '#simid_creative',
+    data: {
+      all_date:[],
+      question_list: [],
+      answer_list:[]
+    },
+    created: function () {
+      axios.get('./test.json?timestamp=' + timestamp).then(function (response) {
+      // 取得完了したらlistリストに代入
+      this.all_date = response.data;
+      for(var i=0;i<this.all_date.length;i++){
+        this.question_list.push(all_date[i].question);
+        this.answer_list.push(all_date[i].answer);
+      }
+      }.bind(this)).catch(function (e) {
+        console.error(e)
+      });
+    },
+    updated: function () {
+
+
+    },
+    methods: {
+
+    }
+  })
+}
+
 
 
 // ボタン処理
