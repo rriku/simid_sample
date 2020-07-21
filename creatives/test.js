dd94072f71e2f8814ec79e8c20b6b5b9bd16d7ab
rriku <68044945+rriku@users.noreply.github.com>
2020-07-21 17:30:58 +0900

    Add files via upload

--- creatives/test.js
+++ creatives/test.js
@@ -27,18 +27,18 @@ function main(){
   new Vue({
     el: '#simid_creative',
     data: {
-      all_date:[],
+      all_data:[],
       question_list: [],
       answer_list:[]
     },
     created: function () {
       axios.get('./test.json?timestamp=' + timestamp).then(function (response) {
       // 取得完了したらlistリストに代入
-      this.all_date = response.data;
-      for(var i=0;i<this.all_date.length;i++){
-        this.question_list.push(this.all_date[i].question);
-        this.answer_list.push(this.all_date[i].answer);
-      }
+      this.all_data = response.data;
+      /* for(var i=0;i<this.all_data.length;i++){
+        this.question_list.push(this.all_data[i].question);
+        this.answer_list.push(this.all_data[i].answers);
+      } */
       }.bind(this)).catch(function (e) {
         console.error(e)
       });
