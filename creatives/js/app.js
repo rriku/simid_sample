9f0f573981c8b01fa5986ac09e67c80f2a8626c4
rriku <68044945+rriku@users.noreply.github.com>
2020-12-09 11:41:43 +0900

    u

--- creatives/js/app.js
+++ creatives/js/app.js
@@ -25,7 +25,7 @@ var deviceType = "";
 var adParameters = "";
 
 // 時間
-var simidDuration = 60000;
+var simidDuration = 100000;
 
 // ピクセルタグ
 var basImgTag = "<img style='height:1px;width:1px;' src='https://in.treasuredata.com/postback/v3/event/simid/simid_survey_result?td_format=pixel&td_write_key=8916/67294c614f548801ce3c9d970c78865b22deb236&survey_id=__SURVEY_ID__&answer_data=__ANSWER_DATA__&td_global_id=td_global_id&td_ip=td_ip&td_ua=td_ua&identifier=__DEVICE_ID__&event=__EVENT__&device=__DEVICE__&vpos=__VPOS__&platform=__PLATFORM__&vid=__VID__' />";
