c8eb13a773fbc4ae517f665cce2c326849fcef02
rriku <68044945+rriku@users.noreply.github.com>
2020-12-18 13:41:00 +0900

    u

--- creatives/js/app.js
+++ creatives/js/app.js
@@ -107,10 +107,9 @@ function main(){
       // console.log(simidController.creativeData.adParameters);
 
       // console.log(simidController.environmentData.duration);
-      
+
       this.show_data = this.all_data.filter(function(data, index){
-        console.log(data.surveyid);
-        console.log(data.surveyid);
+        console.log(data.surveyId);
       });
 
       // タイマー開始
